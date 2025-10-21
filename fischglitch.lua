efce430865277e67d07e41512a75da7eded14254
rybowe <181269264+rybowe@users.noreply.github.com>
2025-10-21 10:54:55 +0200

    Update fischglitch.lua

--- fischglitch.lua
+++ fischglitch.lua
@@ -1,2 +1,21 @@
-print("This script is outdated , wait for another vuln!")
-warn("This script is outdated , wait for another vuln!")
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "TheHubNotice"
+ScreenGui.ResetOnSpawn = false
+ScreenGui.IgnoreGuiInset = true
+ScreenGui.Parent = game.CoreGui
+
+local Frame = Instance.new("Frame")
+Frame.Size = UDim2.new(1, 0, 1, 0)
+Frame.Position = UDim2.new(0, 0, 0, 0)
+Frame.BackgroundColor3 = Color3.new(0, 0, 0)
+Frame.Parent = ScreenGui
+
+local TextLabel = Instance.new("TextLabel")
+TextLabel.Size = UDim2.new(1, 0, 1, 0)
+TextLabel.Position = UDim2.new(0, 0, 0, 0)
+TextLabel.BackgroundTransparency = 1
+TextLabel.Text = "The hub is not available anymore"
+TextLabel.TextColor3 = Color3.new(1, 1, 1)
+TextLabel.TextScaled = true
+TextLabel.Font = Enum.Font.SourceSansBold
+TextLabel.Parent = Frame
